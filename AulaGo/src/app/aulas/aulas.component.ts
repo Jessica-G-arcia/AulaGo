8961e2ccfdbf9af8858568d9b5ba5bd9f384a71b
BeatrizNegraoa <beatrizfranciscanegrao@gmail.com>
2025-04-21 21:50:40 -0300

    feat: tela aulas

--- AulaGo/src/app/aulas/aulas.component.ts
+++ AulaGo/src/app/aulas/aulas.component.ts
@@ -1,11 +1,62 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-aulas',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './aulas.component.html',
   styleUrl: './aulas.component.css'
 })
 export class AulasComponent {
+  aulasConcluidas = [
+    {
+      professor: 'João Pedro',
+      local: 'Sorocaba - SP',
+      horario: '8:00 ás 9:00',
+      idioma: 'Inglês',
+      data: '29/03/2025',
+    },
+    {
+      professor: 'Fernanda Dias',
+      local: 'Sorocaba - SP',
+      horario: '6:00 ás 7:00',
+      idioma: 'Inglês',
+      data: '24/03/2025',
+    },
+    {
+      professor: 'Fernanda Dias',
+      local: 'Sorocaba - SP',
+      horario: '6:00 ás 7:00',
+      idioma: 'Inglês',
+      data: '24/03/2025',
+    },
+    {
+      professor: 'Fernanda Dias',
+      local: 'Sorocaba - SP',
+      horario: '6:00 ás 7:00',
+      idioma: 'Inglês',
+      data: '24/03/2025',
+    },
+    {
+      professor: 'Fernanda Dias',
+      local: 'Sorocaba - SP',
+      horario: '6:00 ás 7:00',
+      idioma: 'Inglês',
+      data: '24/03/2025',
+    },
+    {
+      professor: 'Fernanda Dias',
+      local: 'Sorocaba - SP',
+      horario: '6:00 ás 7:00',
+      idioma: 'Inglês',
+      data: '24/03/2025',
+    },
+    
+  
 
+  ];
+
+ 
 }
