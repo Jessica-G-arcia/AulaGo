095d9b99bdc318e6660290f7b62e4faa125b5126
Picancianmartin <pietra.cmartin@gmail.com>
2025-05-15 08:12:11 -0300

    iniciando

--- AulaGo/src/app/cadastro/cadastro.component.ts
+++ AulaGo/src/app/cadastro/cadastro.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
+  form!: FormGroup;
+
+ngOnInit(): void {
+  this.form = new FormGroup({
+    id: new FormControl(0, Validators.required),
+    nome: new FormControl('', Validators.required),
+    cpf: new FormControl('', Validators.required),
+    telefone: new FormControl('', Validators.required),
+    dataNascimento: new FormControl('', Validators.required),
+    genero: new FormControl('', Validators.required),
+    cep: new FormControl('', Validators.required),
+    cidade: new FormControl('', Validators.required),
+    estado: new FormControl('', Validators.required),
+    endereco: new FormControl('', Validators.required),
+    complemento: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmarSenha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  })
+}
+
+cadastrar(){
+  console.log('Usuário Cadastrado', this.form.value);
+}
 
 }
