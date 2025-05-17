a77e971832926fc9ec101d9945862ea7fc5a1ee1
Jessica-G-arcia <jeh.garcia.jg@gmail.com>
2025-05-17 12:16:31 -0300

    login

--- AulaGo/src/app/login/login.component.ts
+++ AulaGo/src/app/login/login.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { FormsModule, NgForm} from '@angular/forms';
+import { ViewChild } from '@angular/core';
+// import { Usuario } from '../models/Usuario';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, FormsModule, CommonModule], 
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // usuario: Usuario = {
+  //   id: 0,
+  //   email: '',
+  //   senha: ''
+  // };
 
+  @ViewChild('form') form!: NgForm;
+  salvar(){
+    if(this.form.valid){
+      // console.log ('Usuário salvo', this.usuario);
+      alert('Usuário salvo com sucesso!');
+    }
+  }
 }
