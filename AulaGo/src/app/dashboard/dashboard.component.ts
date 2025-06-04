65de926e522a5e314a4dc7f82237a3a88f24c067
Jessica-G-arcia <jeh.garcia.jg@gmail.com>
2025-06-04 20:43:29 -0300

    dash

--- AulaGo/src/app/dashboard/dashboard.component.ts
+++ AulaGo/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuSuperiorComponent } from '../menu-superior/menu-superior.component';
+import { MenuLateralComponent } from '../menu-lateral/menu-lateral.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MenuSuperiorComponent, MenuLateralComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
